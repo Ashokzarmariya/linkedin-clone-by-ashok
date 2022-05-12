16c99552967b1b7ade5b8eead07c2f43318d547f
Ashokzarmariya <ashokzarmariya47@gmail.com>
2022-05-12 18:21:28 +0530

    fixed sum bugs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import type { NextPage } from 'next'
+
 import Head from 'next/head'
 import Image from 'next/image'
 import Navbar from '../components/Navbar'
 
-const Home: NextPage = () => {
+const Home = () => {
   return (
     <div className="space-y-5">
       <Head>
