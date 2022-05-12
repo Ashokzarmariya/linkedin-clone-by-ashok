3187264e72680355c62b9903b4300e3a3b21e055
Ashokzarmariya <ashokzarmariya47@gmail.com>
2022-05-12 18:51:54 +0530

    fixed sum bugs

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,8 +6,8 @@ export default NextAuth({
     // Configure one or more authentication providers
     providers: [
         GoogleProvider({
-            clientId:process.env.GOOGLE_CLIENT_ID || 'ashok',
-            clientSecret:process.env.GOOGLE_CLIENT_SECRET || "zarmariya",
+            clientId:process.env.GOOGLE_CLIENT_ID || '',
+            clientSecret:process.env.GOOGLE_CLIENT_SECRET || "",
         }) ,
        
         // ...add more providers here
